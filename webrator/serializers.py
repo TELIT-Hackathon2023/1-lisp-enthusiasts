899a8860e4e7f38ed4b096ae99f636ce6dde3360
lainczlukas <lukas.laincz@gmail.com>
2023-11-25 19:01:49 +0100

    personas

--- webrator/serializers.py
+++ webrator/serializers.py
@@ -3,3 +3,7 @@ from rest_framework import serializers
 class AnalyticsInputSerializer(serializers.Serializer):
     url = serializers.URLField(required=True)
     code = serializers.CharField(required=False, allow_blank=True)
+
+class PersonaInputSerialier(serializers.Serializer):
+    url = serializers.URLField(required=True)
+    depth = serializers.CharField(required=False, allow_blank=True)
